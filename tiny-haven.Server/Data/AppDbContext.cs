9665ba43fabc2377b9e6ceef1cd5832b92790299
mooliver <oliver.moravek.023@pslib.cz>
2025-12-14 17:37:35 +0100

    Db context

---
+++ tiny-haven.Server/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tiny_haven.Server.Models;
+
+namespace tiny_haven.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Asset> Assets { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<InteractionMap> InteractionMaps { get; set; }
+        public DbSet<InteractionType> InteractionTypes { get; set; }
+        public DbSet<LocationMap> LocationMaps { get; set; }
+        public DbSet<Quest> Quests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Asset>().HasData(
+            //    new Asset
+            //    {
+            //        Name = "Wooden Crate",
+            //        ImageUrl = "images/assets/wooden_crate.png",
+            //        SpanX = 1,
+            //        SpanY = 1,
+            //        Collision = true,
+            //        CategoryId = 1
+            //    }
+            //);
+        }
+    }
+}
