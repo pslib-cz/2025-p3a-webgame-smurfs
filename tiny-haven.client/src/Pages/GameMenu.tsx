8e70a44f011f5c5de819f606c60684e72605d313
mooliver <oliver.moravek.023@pslib.cz>
2026-02-07 00:04:18 +0100

    done

--- tiny-haven.client/src/Pages/GameMenu.tsx
+++ tiny-haven.client/src/Pages/GameMenu.tsx
@@ -1,14 +1,16 @@
 import styles from "./GameMenu.module.css"
+import { useNavigate } from "react-router-dom";
 
 export const GameMenu = () => {
+    const navigate = useNavigate();
 
     return (
         <div className={styles.page}>
-            <div>
-                <img className={styles.headimage} src="images/game_assets/ui/head" alt="hlava" />
+            <div className={styles.gameTitle}>
+                <img className={styles.headimage} src="images/game_assets/ui/head.svg" alt="hlava" />
                 <h1>Tiny Haven</h1>
             </div>
-            <button>Začít hru</button>
+            <button onClick={() => navigate("/play")}>Spustit hru</button>
         </div>
     )
 } 
