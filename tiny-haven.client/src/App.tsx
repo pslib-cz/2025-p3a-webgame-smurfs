9e7a292f12b391ee8749097f43fcfd34cafdc155
Oliver M. <147391014+mooliver@users.noreply.github.com>
2026-02-09 09:44:41 +0100

    fix zoom

--- tiny-haven.client/src/App.tsx
+++ tiny-haven.client/src/App.tsx
@@ -4,6 +4,19 @@ import { MapDisplay } from './Pages/Game';
 import { GameMenu } from './Pages/GameMenu';
 
 function App() {
+    document.addEventListener('wheel', (e) => {
+        if (e.ctrlKey) {
+        e.preventDefault();
+        }
+        }, { passive: false });
+        
+        document.addEventListener('keydown', (e) => {
+        if (e.ctrlKey && (e.key === '+' || e.key === '-' || e.key === '0' ||
+        e.key === '=' || e.keyCode === 107 || e.keyCode === 109)) {
+        e.preventDefault();
+        }
+    });
+
     return (
         <Routes>
             <Route path="/" element={ <GameMenu/> }/>
